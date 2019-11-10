f920a4d0d4a48c8254dd41c36f5299576f132396
Rafał Kuźmiński <rafal.kuzminski.dev@gmail.com>
2019-11-10 16:24:25 +0100

    Add error view

---
+++ app/src/main/java/com/github/coneys/neys/commonUi/ErrorView.kt
@@ -0,0 +1,10 @@
+package com.github.coneys.neys.commonUi
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ErrorView(context: Context?, attrs: AttributeSet?) : ConstraintLayout(context, attrs)
+{
+
+}
