29c4943e057c5bd15953061d9f2c04a6e283e57f
Rafał Kuźmiński <rafal.kuzminski.dev@gmail.com>
2019-11-10 16:35:29 +0100

    Error view / theming upgrade

--- app/src/main/java/com/github/coneys/neys/commonUi/ErrorView.kt
+++ app/src/main/java/com/github/coneys/neys/commonUi/ErrorView.kt
@@ -2,9 +2,12 @@ package com.github.coneys.neys.commonUi
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.constraintlayout.widget.ConstraintLayout
+import com.github.coneys.neys.R
+import com.github.coneys.neys.extensions.layoutInflater
+import com.google.android.material.card.MaterialCardView
 
-class ErrorView(context: Context?, attrs: AttributeSet?) : ConstraintLayout(context, attrs)
-{
-
+class ErrorView(context: Context?, attrs: AttributeSet?) : MaterialCardView(context, attrs) {
+    init {
+        layoutInflater.inflate(R.layout.view_error, this, true)
+    }
 }
